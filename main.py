9278b2422b36335731e8812e409a2be65e0a81ac
Luca Neves <lucatets@gmail.com>
2023-04-18 01:18:54 -0300

    feat(global): adding txt readable template

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ from components.web_server import *
 from config import *
 
 documents, errors = config(open('serverConfig.txt', 'r'))
-print(documents)
 server = WebServer(documents, errors)
 server.start_server()
