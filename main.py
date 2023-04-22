fbf5a4a46e72ba27abfcbbce3fe78b8cb309b3bf
Luca Neves <lucatets@gmail.com>
2023-04-21 21:47:01 -0300

    feat(webserver): handle success, handle error, create friendlly interface

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from components.web_server import *
 from config import *
 
-documents, errors = config(open('serverConfig.txt', 'r'))
+dir_path = os.path.dirname(os.path.realpath(__file__))
+config_file_path = os.path.join(dir_path, 'serverConfig.txt')
+
+documents, errors = config(open(config_file_path, 'r'))
 server = WebServer(documents, errors)
 server.start_server()
