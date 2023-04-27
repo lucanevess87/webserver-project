cb672bd3f222d5a0f55c64a0d700bd63d6113080
maria <maria@clickdb.com.br>
2023-04-27 01:19:53 -0300

    feat: forbidden error

--- components/web_server.py
+++ components/web_server.py
@@ -1,15 +1,19 @@
-from socket import socket, AF_INET, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from threading import Thread
 import os
 from email.utils import formatdate
+import traceback
+
 from components.http_request_content import *
 
+port = 1111
 
 class WebServer:
     def __init__(self, documents, errors):
         self.web_server = socket(AF_INET, SOCK_STREAM)
         self.address = 'localhost'
-        self.web_server.bind((self.address, 1111))
+        self.web_server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.web_server.bind((self.address, port))
         self.web_server.listen()
         self.inspected_folder = documents
         self.errors_folder = errors
@@ -23,21 +27,19 @@ class WebServer:
         # List files of folder => array of files
         self.documents_list = os.listdir(self.inspected_folder)
 
-    # Create handle error function (505,404,400)
     def handle_error(self, socket_client, error_type):
         first_line = f'HTTP/1.1 {error_type} '
-        # Based on error type render
         if error_type == '400':
             first_line += 'Bad Request\r\n'
         elif error_type == '403':
-            first_line += 'Forbidden Request'
+            first_line += 'Fobidden\r\n'
         elif error_type == '404':
             first_line += 'Not Found\r\n'
         elif error_type == '505':
             first_line += 'HTTP Version Not Supported\r\n'
 
         # The proper error file is inside errors folder
-        file = f'{self.errors_folder}/error{error_type}.html'
+        file = f'{self.errors_folder}/html{error_type}.html'
 
         # Designing response
         response = ''
@@ -55,7 +57,6 @@ class WebServer:
         response += content
         socket_client.send(response.encode())
 
-    # Create handle success function (200)
     def handle_success(self, specifications: HTTPRequestContent, socket_client):
         # find target file
         file = self.inspected_folder + specifications.file
@@ -89,7 +90,6 @@ class WebServer:
                 path = '%20'.join(path)
             self.create_interface(socket_client, os.listdir(f'{file}'), path)
 
-    # Create interface - When user open browser server will generate this html content, that allows user to pick some file friendlily
     def create_interface(self, socket_client, documents_list, file):
         # Designing response
         response = ''
@@ -101,35 +101,49 @@ class WebServer:
         socket_client.send(response.encode())
 
         # Designing interface
-        create_index_interface = '<!DOCTYPE html>\r\n'
-        create_index_interface += '<html>\r\n'
-        create_index_interface += '<head>\r\n'
-        create_index_interface += '<title> Files - WebFiles </title>\r\n'
-        create_index_interface += '</head>\r\n'
-        create_index_interface += '\r\n'
-        create_index_interface += '<body>\r\n'
-        create_index_interface += '<h1>Available files <h1>\r\n'
+        crated_index = '<!DOCTYPE html>\r\n'
+        crated_index += '<html>\r\n'
+        crated_index += '<head>\r\n'
+        crated_index += '<title> Files - WebFiles </title>\r\n'
+        crated_index += '</head>\r\n'
+        crated_index += '\r\n'
+        crated_index += '<body>\r\n'
+        crated_index += '<h1>Available files <h1>\r\n'
         if documents_list:
-            create_index_interface += '<ul>\r\n'
+            crated_index += '<ul>\r\n'
             for document in documents_list:
                 if document.split(".")[0] != 'favicon':
-                    create_index_interface += f' <li><a href="http://localhost:1111/{file}{document}"' \
-                        f'>{document.split(".")[0]}</a></li>\r\n'
-            create_index_interface += '<ul>\r\n'
-        create_index_interface += '</body>\r\n'
-        create_index_interface += '</html>\r\n'
+                    crated_index += f' <li><a href="http://localhost:{port}/{file}{document}"' \
+                                    f'>{document.split(".")[0]}</a></li>\r\n'
+            crated_index += '<ul>\r\n'
+        crated_index += '</body>\r\n'
+        crated_index += '</html>\r\n'
 
         # send html interface to be generate on client side (server side rendering)
-        socket_client.send(create_index_interface.encode())
+        socket_client.send(crated_index.encode())
 
-    def handle_http_message(self, http_message):
+    def handle_http_message(self, msg_http):
         # ex: ['GET', '/favicon.ico', 'HTTP/1.1']
-        http_request, file, http_version = http_message.split('\r\n')[
-            0].split(' ')
+        http_request, file, http_version = msg_http.split('\r\n')[0].split(' ')
 
-        # If has index.html at our documents we can already assign value, else it will be generated
+        # dir_path = os.path.dirname(os.path.realpath(__file__))
+        # print(dir_path)
+
+        # If has index.html at our documents we can already assign value else it will be generate
         if file == '/' and 'index.html' in self.documents_list:
+            # page_path = dir_path + '/../pages/index.html'
+            # print(page_path)
             file = 'index.html'
+
+        # if file == '/':
+        #    page_path = dir_path + '/../pages/index.html'
+        #     print(page_path)
+        #    file = 'index.html'
+
+        # if file == '/admin':
+        #     page_path = dir_path + '/../pages/admin/index.html'
+        #     print(page_path)
+        #     file = 'admin/index.html'
 
         # For files like: '/Tame%20Impala%20-%20Borderline%20(Official%20Audio)%20(256%20kbps).mp3'
         if '%20' in file:
@@ -151,7 +165,6 @@ class WebServer:
         elif extension == 'txt':
             extension = 'plain'
 
-        # [http_request (GET, POST, PUT, etc), file, extension (jpeg, mp3, txt), http/1.1]
         return HTTPRequestContent(http_request, file, extension, http_version)
 
     def receive_message(self, socket_client):
@@ -159,28 +172,34 @@ class WebServer:
             http_message = socket_client.recv(2048).decode()
             if http_message:
                 try:
-                    http_content = self.handle_http_message(http_message)
+                    specifications = self.handle_http_message(http_message)
 
-                    if http_content.file == '/':
+                    if specifications.file == '/':
                         self.create_interface(
                             socket_client, self.documents_list, '')
 
                     # http version error
-                    elif http_content.http_version.split('/')[1] != '1.1':
+                    elif specifications.http_version.split('/')[1] != '1.1':
                         self.handle_error(socket_client, 505)
                         print('error 505\n')
 
+                    # forbidden error
+                    elif specifications.file == '/admin':
+                        self.handle_error(socket_client, 403)
+                        print('error 403\n')
+
                     # file not found error
-                    elif http_content.file[1:].split('/')[0] not in self.documents_list:
+                    elif specifications.file[1:].split('/')[0] not in self.documents_list:
                         self.handle_error(socket_client, 404)
                         print('error 404\n')
 
                     else:
-                        self.handle_success(http_content, socket_client)
+                        self.handle_success(specifications, socket_client)
                         print('200 ok\n')
                 except:
                     # server lost error
                     print('error 400\n')
+                    traceback.print_exc()
                     self.handle_error(socket_client, 400)
 
     def start_server(self):
